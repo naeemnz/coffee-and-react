6ab51ec676412c922722107e19815aaba5590532
Nazifa <nazifanaimie@gmail.com>
2025-10-11 21:54:48 +0200

    basic create react app and vite files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h2>Coffee and React</h2>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h2>Coffee and React with Vite | NZ</h2>
+  )
+}
+
+export default App
